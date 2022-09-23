e45422430b6209eb8dc5d4710fa5373b7d9c4cf2
William Harris <17HarriW@fulford.york.sch.uk>
2022-09-23 15:20:36 +0100

    Debugging :)

--- PizzaList/Program.cs
+++ PizzaList/Program.cs
@@ -10,6 +10,35 @@ namespace PizzaList
     {
         static void Main(string[] args)
         {
+            List<string> toppings = new List<string> { "Pepperoni","Margherita","Hawaiian"};
+
+            List<string> selections = ChooseFrom(toppings);
+            Console.WriteLine(selections);
+            Console.ReadLine();
+        }
+
+        private static List<string> ChooseFrom(List<string> toppings)
+        {
+            string selection = "";
+            List<string> selections = new List<string> { };
+            while (selection != "q")
+
+            {
+                int i = 1;
+                foreach (string topping in toppings)
+                {
+                    Console.WriteLine(i + ". " + topping);
+                    i++;
+                }
+                Console.WriteLine("Enter the corresponding value, q to quit");
+
+                selection = Console.ReadLine();
+                if (selection != "q")
+                {
+                    selections.Add(toppings[(int.Parse(selection) - 1)]);
+                }
+            }
+            return selections;
         }
     }
 }
